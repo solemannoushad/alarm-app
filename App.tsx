cc321a0897b0f39ab276f1e4b295846661230557
solemannoushad <solemannoushad31@gmail.com>
2025-08-10 03:37:44 -0700

    app created

--- App.tsx
+++ App.tsx
@@ -6,15 +6,18 @@
  */
 
 import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { StatusBar, StyleSheet, Text, useColorScheme, View } from 'react-native';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
     <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
+      {/* <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} /> */}
+      {/* <NewAppScreen templateFileName="App.tsx" /> */}
+      <Text style={{ color: isDarkMode ? 'white' : 'black', fontSize: 20, textAlign: 'center', marginTop: 50 }}>
+        Welcome to the New App Screen!
+      </Text>
     </View>
   );
 }
